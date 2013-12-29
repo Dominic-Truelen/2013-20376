f45257e1c44863bfa1f5d571eeb80ae28a160ad4
Tom-2013-06417 <airbreather7@gmail.com>
2013-12-30 02:28:14 +0800

    Transformed loginPage into Class
    in order to abide by the rulez

--- GUI_Basic_1.py
+++ GUI_Basic_1.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python																# Allows the use of Non-ASCII characters in window
 # -*- coding: utf-8 -*-
  
-from CC import *																# pack() is for stacking, while place() is for a more
+from CC import validation, CC													# pack() is for stacking, while place() is for a more
 from Tkinter import *															# accurate placing of widgets. grid() is for tables
 import os, sys, glob
-
-val = validation()																# Initial functions for the topLayer
-loginCC = CC()
-lg = login()
 
 defaultEntryStyle = ("Tahoma", 12)												# Initial font settings for styling
 defaultLabelStyle = ("Tahoma", 9)
@@ -15,6 +11,7 @@ signatureColor = "#AB3700"														# Shortcut labels for custom color styli
 toplayerColor = "#433833"
 backgroundColor = "#FFEEE5"
 
+'''
 def guiValidation(event):														# Used alternatively for the button press
 	loginButton.flash()															# Basically executes the same validate method, but
 	if usernameInput.get() == "":												# this "event" accepts the <Return> key as input
@@ -32,61 +29,70 @@ def guiValidation(event):														# Used alternatively for the button press
 				print "SUCCESSFUL"
 			else:
 				print "INVALID PASSWORD"
+'''
 
-def accept():
-	responses=["YOU DID NOT INPUT A USERNAME", "YOU DID NOT INPUT A PASSWORD", "USERNAME NOT FOUND", "INVALID PASSWORD"]
-	loginCC.set_name(usernameInput.get())
-	loginCC.set_password(passwordInput.get())
-	if val.guiv(loginCC.get_name(), loginCC.get_password()) in responses:
-		verifyLabel.config(text=val.guiv(loginCC.get_name(), loginCC.get_password()))
-	else:
-		pass
-	
+#All the other codes / widgets for the Login Page (still inc)
+
+class Page(Frame):																# Parent Frame that lets all children have show()
+	def __init__(self):
+		Frame.__init(self)
+	def show(self):
+		self.lift()
+
+class loginPage(Page):															# The login class!
+	def __init__(self):	
+		Frame.__init__(self)
+		Window.wm_title('Welcome to Caffy')										# Initial title text in the title bar
+		
+		self.val = validation()													# Initial functions for the topLayer
+		self.loginCC = CC()
+		
+		topLayer = Frame(self, height=80, width=1000, bg=toplayerColor)			# Code here for the Log-in classes
+		topLayer.pack()
+
+		usernameLabel = Label(topLayer, text="Username", fg="#FFF", bg=toplayerColor, font=defaultLabelStyle)
+		usernameLabel.place(anchor=CENTER, relx=0.5379, rely=0.28)
+		passwordLabel = Label(topLayer, text="Password", fg="#FFF", bg=toplayerColor, font=defaultLabelStyle)
+		passwordLabel.place(anchor=CENTER, relx=0.7163, rely=0.28)
+
+		usernameInput = Entry(topLayer, width=17, font=defaultEntryStyle, relief=FLAT)	# Entry field for the username
+		usernameInput.place(anchor=CENTER,relx=0.59, rely=0.59)
+		usernameInput.focus()													# Puts the cursor automatically at the username field
+
+		passwordInput = Entry(topLayer, width=17, show="•", font=defaultEntryStyle, relief=FLAT)	#Entry field for the password
+		passwordInput.place(anchor=CENTER, relx=0.77, rely=0.59)
+		
+		verifyLabel = Label(topLayer, text="", fg="#FFF000", bg=toplayerColor, font=defaultLabelStyle)
+		verifyLabel.place(anchor=E, relx=0.5, rely=0.59)
+		
+		loginButton = Button(topLayer, text="Log In", width=7, height=1, font=("Tahoma", 9, "bold"), relief=GROOVE, fg="#FFFFFF", bg=signatureColor, command=lambda: _accept(usernameInput.get(), passwordInput.get()))
+		#usernameInput.bind("<Return>", guiValidation)		#If user presses enter key on usr, execute the Login (This is a secret function)
+		#passwordInput.bind("<Return>", guiValidation)		#If user presses enter key on pwd, execute the Login (This is also a secret function)
+		loginButton.place(anchor=CENTER, relx=0.904, rely=0.59)
+
+		midLayer = Frame(self, width=1000, height=490, bg="#FFF")				# Code here for the CREATE class
+		midLayer.pack()
+
+		bottomLayer = Frame(self, width=1000, height=30, bg=toplayerColor)		# Code here for the extra details
+		bottomLayer.pack()	
+		
+		def _accept(userName, passWord):		
+			self.responses=["YOU DID NOT INPUT A USERNAME", "YOU DID NOT INPUT A PASSWORD", "USERNAME NOT FOUND", "INVALID PASSWORD"]
+			self.loginCC.set_name(userName)
+			self.loginCC.set_password(passWord)
+			if self.val.guiv(self.loginCC.get_name(), self.loginCC.get_password()) in self.responses:			
+				verifyLabel.config(text=self.val.guiv(self.loginCC.get_name(), self.loginCC.get_password()))
+			else:
+				print "SUCCESSFUL"
+
+
 Window = Tk()          		 													# Creates an empty window			
 Window.geometry('1000x600+170+80')
 Window.resizable(0,0)			 												# Do not resize the window, ever 	
 Window.wm_iconbitmap('CoffeeCup.ico')											# Add a little mug icon over the top left corner
-Window.wm_title('Welcome to Caffy')												# Initial title text in the title bar
-
-#All the other codes / widgets for the Login Page (still inc)
-
-
-loginPage = Frame(Window)														# Frame that contains all of the elements of the Login page
-loginPage.pack()
-
-
-topLayer = Frame(loginPage, height=80, width=1000, bg=toplayerColor)			# Code here for the Log-in classes
-topLayer.pack()
-
-usernameLabel = Label(topLayer, text="Username", fg="#FFF", bg=toplayerColor, font=defaultLabelStyle)
-usernameLabel.place(anchor=CENTER, relx=0.5379, rely=0.28)
-passwordLabel = Label(topLayer, text="Password", fg="#FFF", bg=toplayerColor, font=defaultLabelStyle)
-passwordLabel.place(anchor=CENTER, relx=0.7163, rely=0.28)
-
-verifyLabel = Label(topLayer, text="", fg="#FFF000", bg=toplayerColor, font=defaultLabelStyle)
-verifyLabel.place(anchor=E, relx=0.5, rely=0.59)
-
-usernameInput = Entry(topLayer, width=17, font=defaultEntryStyle, relief=FLAT)	# Entry field for the username
-usernameInput.place(anchor=CENTER,relx=0.59, rely=0.59)
-usernameInput.focus()															# Puts the cursor automatically at the username field
-
-passwordInput = Entry(topLayer, width=17, show="•", font=defaultEntryStyle, relief=FLAT)	#Entry field for the password
-passwordInput.place(anchor=CENTER, relx=0.77, rely=0.59)
-
-loginButton = Button(topLayer, text="Log In", width=7, height=1, font=("Tahoma", 9, "bold"), relief=GROOVE, fg="#FFFFFF", bg=signatureColor, command=accept)
-usernameInput.bind("<Return>", guiValidation)		#If user presses enter key on usr, execute the Login (This is a secret function)
-passwordInput.bind("<Return>", guiValidation)		#If user presses enter key on pwd, execute the Login (This is also a secret function)
-loginButton.place(anchor=CENTER, relx=0.904, rely=0.59)
-
-
-midLayer = Frame(loginPage, width=1000, height=490, bg="#FFF")					# Code here for the CREATE class
-midLayer.pack()
-
-bottomLayer = Frame(loginPage, width=1000, height=30, bg=toplayerColor)			# Code here for the extra details
-bottomLayer.pack()
-
+Main = loginPage()
+Main.place(x=0, y=0)
 Window.mainloop()																# Executes code above in a loop
-#loginWindow.destroy()
 
 os.remove('CC.pyc')																# Removes temporary files
 #os.remove('Main.pyc')	
